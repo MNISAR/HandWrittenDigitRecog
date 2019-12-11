43f0dea77cac408fdb112ac08433bc85fbf9a6d7
MNISAR <nisarmonil@gmail.com>
2019-12-11 16:16:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -44,4 +44,4 @@ def prediction():
     pr = m.predict(image)
     return render_template('result.html', prediction=str(pr), img="static/result.png?"+str(datetime.now()), original_img="static/{}.png?".format(pr)+str(datetime.now()))
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run()
