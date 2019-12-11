17b53f5ad0a4e50d46add59640efc750f113c187
MNISAR <nisarmonil@gmail.com>
2019-12-11 01:37:58 -0700

    "Final"

--- main.py
+++ main.py
@@ -4,38 +4,77 @@ Created on Fri Dec  6 12:51:43 2019
 
 @author: monil
 """
+import tensorflow as tf
+graph = tf.get_default_graph()
+from keras.layers import LeakyReLU
+from keras.layers.normalization import BatchNormalization
+from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+#from tensorflow.contrib.layers import dropout as Dropout
+from keras.optimizers import RMSprop
+import keras
 import numpy as np
 import pandas as pd
-from PIL import Image
+import matplotlib.pyplot as plt
 import PIL
 
 class MODEL:
     def make_model(self):
-        df = pd.read_csv("data/train.csv")
-        y_train = df['label'].to_numpy()
-        X_train = df.drop(['label'], axis=1).to_numpy()
-
-        from sklearn.neighbors import KNeighborsClassifier
-        model = KNeighborsClassifier(n_neighbors=5)
-        model.fit(X_train, y_train)
-        
-        self.model = model
+        #keras.backend.clear_session()
+        modelo = Sequential()
+        modelo.add(Conv2D(64,  (3,3), padding='same', input_shape=(28, 28, 1)))
+        modelo.add(BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer="uniform"))
+        modelo.add(LeakyReLU(alpha=0.1))
+        modelo.add(MaxPooling2D(2, 2))
+        modelo.add(Dropout(0.2))
+        modelo.add(Conv2D(128, (3,3), padding='same'))
+        modelo.add(BatchNormalization(momentum=0.2, epsilon=1e-5, gamma_initializer="uniform"))
+        modelo.add(LeakyReLU(alpha=0.1))
+        modelo.add(Conv2D(128, (3,3), padding='same'))
+        modelo.add(BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer="uniform"))
+        modelo.add(LeakyReLU(alpha=0.1))
+        modelo.add(MaxPooling2D(2,2))
+        modelo.add(Dropout(0.2))
+        modelo.add(Conv2D(256, (3,3), padding='same'))
+        modelo.add(BatchNormalization(momentum=0.2, epsilon=1e-5, gamma_initializer="uniform"))
+        modelo.add(LeakyReLU(alpha=0.1))
+        modelo.add(Conv2D(256, (3,3), padding='same'))
+        modelo.add(BatchNormalization(momentum=0.1, epsilon=1e-5, gamma_initializer="uniform"))
+        modelo.add(LeakyReLU(alpha=0.1))
+        modelo.add(MaxPooling2D(2,2))
+        modelo.add(Dropout(0.2))
+        modelo.add(Flatten())
+        modelo.add(Dense(256))
+        modelo.add(LeakyReLU(alpha=0.1))
+        modelo.add(BatchNormalization())
+        modelo.add(Dense(10, activation='softmax'))
+        return modelo
 
     def preprocess_image(self,image):
         pass
 
+    def loadmodel(self,model_path="models/model.h5"):
+        self.model = self.make_model()
+        self.model.load_weights(model_path)
+        initial_learningrate=2e-3
+        self.model.compile(loss= 'categorical_crossentropy', optimizer= keras.optimizers.RMSprop() , metrics=['accuracy'])
+
     def image_show(self,image):
+        from PIL import Image
         image.reshape((28,28,1))
         image = image.astype('int8')
-        img = PIL.Image.fromarray(image)
+        img = Image.fromarray(image)
         img.show()
 
 
     ### load data and train model
-    def predict(self,image):
-        prediction = self.model.predict(image)
-        image = image.reshape((28,28))
-        im = Image.fromarray(image)
-        im.save("static/abc.png")
+    def predict(self,image): # assuming the image size is 28 X 28
+        test = image.reshape(1, 28, 28, 1)
+        test = test.astype('float32')
+        test = test / 255.0
+        print(test.shape)
+        with graph.as_default():
+            prediction = np.argmax(self.model.predict(test))
+        print("Prediction: ",prediction)
         return prediction
-
--- server.py
+++ server.py
@@ -4,13 +4,22 @@ Created on Sat Dec  7 17:25:38 2019
 
 @author: monil
 """
-from flask import Flask,render_template,request,jsonify
 
+from flask import Flask,render_template,request,jsonify
+import base64
+from PIL import Image
+import PIL
+from io import BytesIO
+from main import MODEL
 import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
 app = Flask(__name__,static_url_path='/static')   # Flask constructor 
-
+m = MODEL()
+m.loadmodel()
 # A decorator used to tells the application 
 # which URL is associated function 
 @app.route('/')      
@@ -19,6 +28,21 @@ def hello():
     with open('templates/mainPage.html') as f:
     	doc = f.read()
     return doc
+  
+    
+@app.route('/process_request', methods=['POST', 'GET'])
+def prediction():
+    imagefile = request.form['imagefile']
+    string = "".join(imagefile.split(",")[1:])
+    image = base64.b64decode(string)
+    image = mpimg.imread(BytesIO(image), format='jpg')
+    Image.fromarray(np.array(image)).save("static/result.png", format="png")
+    image = np.array(image)[:,:,3]
+    im = Image.fromarray(image)
+    im.thumbnail((28,28))
+    image = np.array(im)
+    pr = m.predict(image)
+    return render_template('result.html', prediction=str(pr), img="static/result.png?"+str(datetime.now()))
  
 if __name__=='__main__': 
    app.run(debug=True, host='0.0.0.0')
